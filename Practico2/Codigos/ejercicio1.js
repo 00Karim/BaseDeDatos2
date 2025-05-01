daa574baf636916215677b0a5cf5b6db6074b22b
00Karim <karimveleza@gmail.com>
2025-05-01 17:18:51 -0300

    Ejercicios 3,4,5
    Cambie el tipo de archivo de .mongodb a js para poder usarlos fuera de
    el entorno de VSCode y usarlos en mongosh. Agregamos del ejercicio 3 al
    5.

--- Practico2/Codigos/ejercicio1.js
+++ Practico2/Codigos/ejercicio1.js
@@ -1,9 +1,9 @@
-use ('empresa')
+use empresa;
 db.empleados.insertMany([
     {nombre: "Marcos", edad: 20, puesto: "Pasante"},
     {nombre: "Pedro", edad: 48, puesto: "Reponedor"},
     {nombre: "Maria", edad: 29, puesto: "Cajero"}
-])
-db.empleados.updateOne({nombre: "Pedro"}, { $set: {edad: 109}})
-db.empleados.deleteOne({puesto: "Pasante"})
-db.empleados.find().pretty()
+]);
+db.empleados.updateOne({nombre: "Pedro"}, { $set: {edad: 109}});
+db.empleados.deleteOne({puesto: "Pasante"});
+db.empleados.find().pretty();
--- Practico2/Codigos/ejercicio2.js
+++ Practico2/Codigos/ejercicio2.js
@@ -1,5 +1,5 @@
-use ('empresa')
+use empresa;
 db.empleados.find(
     {edad: {$gt: 25, $lt: 40}}, 
     {nombre: 1, edad: 1}
-)
+);
