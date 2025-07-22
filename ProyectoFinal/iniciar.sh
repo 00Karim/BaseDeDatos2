c98804f788a58e236a71c64512bfc06d2c676fcc
00Karim <karimveleza@gmail.com>
2025-07-22 18:37:22 -0300

    Falto borrar algo en el iniciar.sh, listo

--- ProyectoFinal/iniciar.sh
+++ ProyectoFinal/iniciar.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "Cargando..."
+echo "Cargando, espere unos minutos..."
 # sudo docker-compose down 
 #> /dev/null 2>&1
 # construir la imagen y hacemos que no se vea ninguno de los mensajes que se muestran normalmente asi queda mas bonito
-sudo docker-compose up --build -d 
-#> /dev/null 2>&1
+sudo docker-compose up --build -d > /dev/null 2>&1
 # clear
 # correr el front
 python3 frontend/inicio.py
